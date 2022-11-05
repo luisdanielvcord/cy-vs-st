2bc5eebd564df3e6ace2d8267d1d5add7669d658
luisdanielvcord <luisdanielvasquez@hotmail.es>
2022-11-04 21:55:52 -0500

    supertest completed

--- supertest_tmdb.spec.js
+++ supertest_tmdb.spec.js
@@ -8,58 +8,48 @@ describe("test suite", () => {
     var sessionId
     var test
 
-    before('auth', () => {
+    before('auth', async () => {
 
-        request('https://api.themoviedb.org/3/')
+        await request('https://api.themoviedb.org/3/')
             .get('authentication/token/new?api_key=' + data.key)
-            .end((err, res) => {
+            .expect((res) => {
                 reqToken = res.body.request_token
-                expect(res.statusCode).to.be.equal(200)
-
-                request('https://api.themoviedb.org/3/')
-                    .post('authentication/token/validate_with_login?api_key=' + data.key)
-                    .send({
-                        "username": data.username,
-                        "password": data.password,
-                        "request_token": reqToken
-                    })
-                    .end((err, res) => {
-                        expect(res.statusCode).to.be.equal(200)
-
-                        request('https://api.themoviedb.org/3/')
-                            .post('authentication/session/new?api_key=' + data.key)
-                            .send({
-                                "username": data.username,
-                                "password": data.password,
-                                "request_token": reqToken
-                            })
-                            .end((err, res) => {
-                                sessionId = res.body.session_id
-                                expect(res.statusCode).to.be.equal(200)
-
-                                request('https://api.themoviedb.org/3/')
-                                    .post('movie/12335/rating?api_key=' + data.key + '&session_id=' + sessionId)
-                                    .send({
-                                        "value": 8.0
-                                    })
-                                    .end((err, res) => {
-                                        expect(res.statusCode).to.be.equal(201)
-                                    })
-
-                                request('https://api.themoviedb.org/3/')
-                                    .delete('movie/12335/rating?api_key=' + data.key + '&session_id=' + sessionId)
-                                    .end((err, res) => {
-                                        expect(res.statusCode).to.be.equal(200)
-                                    })
-                            })
-                    })
             })
+            .expect(200)
+
+        await request('https://api.themoviedb.org/3/')
+            .post('authentication/token/validate_with_login?api_key=' + data.key)
+            .send({
+                "username": data.username,
+                "password": data.password,
+                "request_token": reqToken
+            })
+            .expect(200)
+
+        await request('https://api.themoviedb.org/3/')
+            .post('authentication/session/new?api_key=' + data.key)
+            .send({
+                "username": data.username,
+                "password": data.password,
+                "request_token": reqToken
+            })
+            .expect((res) => {
+                sessionId = res.body.session_id
+            })
+            .expect(200)
     })
 
-    it('create and delete rating', () => {
-        console.log(test)
-        console.log(sessionId)
-        console.log(reqToken)
+    it('create and delete rating', async () => {
 
+        await request('https://api.themoviedb.org/3/')
+            .post('movie/12335/rating?api_key=' + data.key + '&session_id=' + sessionId)
+            .send({
+                "value": 8.0
+            })
+            .expect(201)
+
+        await request('https://api.themoviedb.org/3/')
+            .delete('movie/12335/rating?api_key=' + data.key + '&session_id=' + sessionId)
+            .expect(200)
     })
 })
